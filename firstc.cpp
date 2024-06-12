933d748751a3c82b4b0314afd3b78b1bd7c68918
khalidKnox <poluoch204@gmail.com>
2024-06-12 13:34:58 +0300

    Add files via upload

---
+++ firstc.cpp
@@ -0,0 +1,16 @@
+#include "firstc.h"
+
+#include <stdio.h>
+#include <math.h>
+
+ main() {
+   float number;
+   float answer;
+   char name;
+
+    printf("Enter any number:  ");
+scanf("%f,&number");
+answer=sqrt(number);
+printf("The result is%2f\n",answer);
+//return 0;
+}
